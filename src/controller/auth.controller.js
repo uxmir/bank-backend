80c4b6f9681109c59dc919af63c18865b9f6c623
Mir Moniruzzaman <mirmuniruzzaman303@gmail.com>
2026-03-05 21:53:43 +0600

    login controller is done

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -39,6 +39,49 @@ const userRegisterController = async (req, res) => {
   }
 };
 
+//login controller
+const userLoginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(401).json({
+        message: "user is invalid please Sign up",
+        success: false,
+        status: "failed",
+      });
+    }
+    //verify password
+    const isValidPassword = await user.comparePassword(password);
+    if (!isValidPassword) {
+      return res.status(401).josn({
+        message: "password or email is invalid",
+        success: false,
+        status: "failed",
+      });
+    }
+    const token = await jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token);
+    return res.status(200).json({
+      user: {
+        _id: user._id,
+        email: user.email,
+        name: user.name,
+      },
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "there is somthing error",
+      status: "Faild",
+      success: false,
+      error,
+    });
+  }
+};
 module.exports = {
   userRegisterController,
+  userLoginController,
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,4 +2,5 @@ const express=require('express')
 const router=express.Router()
 const authController=require('../controller/auth.controller')
 router.post('/register',authController.userRegisterController)
+router.post('/login',authController.userLoginController)
 module.exports=router
