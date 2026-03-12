118d3212a11b933b01d2eb695866c4f8e298b7e6
Mir Moniruzzaman <mirmuniruzzaman303@gmail.com>
2026-03-12 20:09:26 +0600

    create transaction controller is ongoing

--- src/controller/transactionController.js
+++ src/controller/transactionController.js
@@ -1,10 +1,68 @@
-const transactionModel=require('../models/transaction.model')
+const transactionModel = require("../models/transaction.model");
+const accountModel = require("../models/account.model");
+const transactionController = async (req, res) => {
+  try {
+    const { fromAccount, toAccount, status, amount, itempoitencyKey } =
+      req.body;
+    if (!fromAccount || !toAccount || !status || !amount || !itempoitencyKey) {
+      return res.status(400).json({
+        message:
+          "fromAccount,toAccount,status,amount, itempoitencyKey is required",
+        success: false,
+      });
+    }
+    const fAccount = await accountModel.findOne({
+      _id: fromAccount,
+    });
+    const tAccount = await accountModel.findOne({
+      _id: toAccount,
+    });
+    if (!fAccount || !tAccount) {
+      return res.status(400).json({
+        message: "faccount or taccount is not exists",
+        success: false,
+      });
+    }
+    const isTransactionExists = await transactionModel.findOne({
+      itempoitencyKey: itempoitencyKey,
+    });
+    if (isTransactionExists) {
+      if (isTransactionExists?.status === "COMPLETED") {
+        return res.status(200).json({
+          message: "Transaction has completed",
+        });
+      }
+      if (isTransactionExists?.status === "PENDING") {
+        return res.status(200).json({
+          message: "Transaction still processing",
+        });
+      }
+      if (isTransactionExists?.status === "FAILED") {
+        return res.status(500).json({
+          message: "Transaction has been failed",
+        });
+      }
+      if (isTransactionExists?.status === "REVERSED") {
+        return res.status(500).json({
+          message: "Transaction is reversed please try again",
+        });
+      }
+    }
+    //checking account status
+    if (fAccount?.status !== "ACTIVE" || tAccount?.status !== "ACTIVE") {
+      return res.status(500).json({
+        message: "fAccount or tAccount must be active",
+        success: false,
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: "there is something wrong in transactionController",
+      error,
+    });
+  }
+};
 
-const transactionController=async(req,res)=>{
- try {
-    
- } catch (error) {
-    
- }   
-}
-
+module.exports = {
+  transactionController,
+};
--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const router=express.Router()
-const authMiddleWare=require('../middleware/auth.middleware')
-router.post('/',authMiddleWare.authMiddleware)
+const authMiddleWare=require('../middleware/auth.middleware');
+const transactionController=require('../controller/transactionController')
+router.post('/',authMiddleWare.authMiddleware,transactionController.transactionController)
 module.exports=router
